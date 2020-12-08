ff411c02d5544e07b0504aa9c3f9120ff9cf9b9b
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-12-09 01:05:10 +0300

    add: done tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -159,8 +159,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -180,8 +180,8 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / (10 * pow === 0 ? 1 : 10 ** pow)) * (10 * pow === 0 ? 1 : 10 ** pow);
 }
 
 /**
@@ -201,8 +201,21 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(num) {
+  let z = true;
+  if (num < 2) {
+    z = false;
+  } else if (num % 2 === 0 && num !== 2) {
+    z = false;
+  } else {
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+      if (num % i === 0) {
+        z = false;
+        break;
+      }
+    }
+  }
+  return (z);
 }
 
 /**
@@ -220,8 +233,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return +value < Infinity ? +value : def;
 }
 
 module.exports = {
